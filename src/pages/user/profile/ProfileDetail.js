bf5d6fe049655e5ce1c72117499a05c4c8a63872
Nguyễn Tấn Phon <123572919+Meowinosis@users.noreply.github.com>
2024-01-11 15:08:00 +0700

    Update ProfileDetail.js

--- src/pages/user/profile/ProfileDetail.js
+++ src/pages/user/profile/ProfileDetail.js
@@ -7,53 +7,27 @@ import {useDispatch, useSelector} from "react-redux";
 export default function ProfileDetail() {
     const dispatch = useDispatch();
     let currentUser = JSON.parse(localStorage.getItem("currentToken"));
-    console.log(currentUser.id);
     const { id } = useParams();
-    // Khởi tạo state để lưu trữ dữ liệu từ axios
     const [userInfo, setUserInfo] = useState(null);
 
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        // Hàm async để gửi yêu cầu axios
         const fetchData = async () => {
             try {
-                // Gửi yêu cầu axios
                 const response = await axios.get(`http://localhost:8080/users/${id}`);
 
-                // Lưu trữ dữ liệu vào state
                 setUserInfo(response.data);
-                setLoading(false); // Đánh dấu là đã tải xong
+                setLoading(false);
             } catch (error) {
-                // Xử lý lỗi nếu có
                 setError(error);
                 setLoading(false);
             }
         };
-        fetchData();// Gọi hàm fetchData khi component được mount
-    }, []); // Tham số thứ hai là một mảng rỗng để đảm bảo useEffect chỉ chạy một lần khi component được mount
+        fetchData();
+    }, []); 
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         dispatch(getUser(currentUser.id));
-    //     }
-    //     fetchData();
-    // }, [dispatch, currentUser])
-
-    // const user = useSelector((state) => state.users.list.find((u) => u.id === currentUser.id));
-    // useEffect(() => {
-    //     if (user) {
-    //         setUserInfo({
-    //             username: user.username,
-    //             fullName: user.fullName,
-    //             dateOfBirth: user.dateOfBirth,
-    //             address: user.address,
-    //             phone: user.phone,
-    //             email: user.email,
-    //         });
-    //     }
-    // }, [user]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -61,7 +35,6 @@ export default function ProfileDetail() {
             ...prevUserInfo,
             [name]: value,
         }));
-        console.log(userInfo);
     };
 
     const handleSaveChanges = async () => {
