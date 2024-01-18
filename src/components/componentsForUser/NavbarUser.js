88c8e6bc8375c4ff1cb3d4859e7f7e198450c746
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2024-01-18 23:25:16 +0700

    Đặt link home về trang /house

--- src/components/componentsForUser/NavbarUser.js
+++ src/components/componentsForUser/NavbarUser.js
@@ -11,7 +11,7 @@ export default function NavbarUser() {
             <div className="main-navigation">
                 <div className="container">
                     <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
-                        <a className="navbar-brand" href="/">
+                        <a className="navbar-brand" href="/house">
                             <img src="../assets/img/logo.png" alt=""/>
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
